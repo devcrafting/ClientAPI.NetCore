c42387a9b2077288dc8a1e41ae28739bec9f23bd
Clément Bouillier <clement.bouillier@devcrafting.com>
2017-11-14 22:48:19 +0100

    Test failing with expected behavior

---
+++ test/EventStore.ClientAPI.NetCore.Tests/when_keep_reconnecting_without_server_responding.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using NUnit.Framework;
+using EventStore.ClientAPI;
+using EventStore.ClientAPI.Internal;
+using EventStore.ClientAPI.Exceptions;
+using EventStore.Core.Tests.ClientAPI.Helpers;
+using System.Threading.Tasks;
+
+namespace EventStore.Core.Tests.ClientAPI
+{
+    public class when_keep_reconnecting_without_server_responding
+    {
+        [Test]
+        public void read_events_should_timeout()
+        {
+            var settings =
+                ConnectionSettings.Create()
+                    .EnableVerboseLogging()
+                    .KeepReconnecting()
+                    .SetOperationTimeoutTo(TimeSpan.FromSeconds(1))
+                    .WithConnectionTimeoutOf(TimeSpan.FromSeconds(10))
+                    .SetReconnectionDelayTo(TimeSpan.FromMilliseconds(0))
+                    .FailOnNoServerResponse();
+
+            using (var connection = EventStoreConnection.Create(settings, TestNode.BlackHole))
+            {
+                connection.ConnectAsync().Wait();
+                Assert.Throws<OperationTimedOutException>(() =>
+                {
+                    var task = connection.ReadAllEventsForwardAsync(Position.Start, 100, false);
+                    // nunit timeout attribute still not working with dotnet core
+                    Exception expectedException = null;
+                    Task.WhenAny(task, Task.Delay(10000))
+                        .ContinueWith(t => expectedException = t.Result.Exception).Wait();
+                    if (expectedException != null)
+                        throw expectedException.InnerException;
+                });
+            }
+        }
+    }
+}
